ffe2ac51b73704cfdba52cc92fe69931c4f6f9f4
sam1605 <69100119+sam1605@users.noreply.github.com>
2025-04-14 11:29:42 +0530

    Update StartPage.jsx

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../app.css";
 import Swal from 'sweetalert2'
 import {FaRedo } from "react-icons/fa"
 
