dbaa37f912c51ec82212d7911e73337f5e6e8cad
sam1605 <69100119+sam1605@users.noreply.github.com>
2025-04-14 11:30:00 +0530

    Update VersePage.jsx

--- src/components/VersePage.jsx
+++ src/components/VersePage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../app.css";
 
 const VersePage = () => {
   const [verse, setVerse] = useState(null);
